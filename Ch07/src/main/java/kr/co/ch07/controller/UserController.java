f59dae5d0c5f59bdd5e2ac354ef993337ed7ff7e
jangbigom91 <jangbigom91@gmail.com>
2020-09-03 12:48:50 +0900

    spring 09/03 ch07

--- Ch07/src/main/java/kr/co/ch07/controller/UserController.java
+++ Ch07/src/main/java/kr/co/ch07/controller/UserController.java
@@ -6,7 +6,6 @@ import javax.inject.Inject;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
