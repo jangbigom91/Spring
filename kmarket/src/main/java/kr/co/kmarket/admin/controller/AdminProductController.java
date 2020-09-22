1d2a09cd91817a630b7db44d2681b8082d9bec8d
jangbigom91 <jangbigom91@gmail.com>
2020-09-22 14:25:14 +0900

    spring 페이징처리 kmarket 09/22

--- kmarket/src/main/java/kr/co/kmarket/admin/controller/AdminProductController.java
+++ kmarket/src/main/java/kr/co/kmarket/admin/controller/AdminProductController.java
@@ -1,13 +1,33 @@
 package kr.co.kmarket.admin.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import kr.co.kmarket.admin.service.AdminProductService;
+import kr.co.kmarket.vo.ProductsVo;
 
 @Controller
 public class AdminProductController {
+
+	@Autowired
+	private AdminProductService service;
 	
 	@GetMapping("/admin/product/list")
-	public String list() {
+	public String list(Model model, String pg) {
+		
+		int start = service.getLimitStart(pg);
+		int total = service.selectCountProducts();
+		int pageEnd = service.getPageEnd(total);
+		
+		List<ProductsVo> products = service.selectProducts(start);
+		model.addAttribute("products", products);
+		model.addAttribute("pageEnd", pageEnd);
+		model.addAttribute("currentPg", pg);
+		
 		return "/admin/product/list";
 	}
 	
@@ -15,4 +35,5 @@ public class AdminProductController {
 	public String register() {
 		return "/admin/product/register";
 	}
+	
 }
